f49f6f8faf84835005efeae3082791788017a002
jsiny <juju.siny@gmail.com>
2019-02-21 21:19:09 +0000

    Add test for valid user submission

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,4 +13,14 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {name: "Example User", email: "foo@bar.com", password: "foobar", password_confirmation: "foobar"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert flash.any?
+  end
+
 end
