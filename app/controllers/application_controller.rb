de1195704f2ccff65d6bf18bf40d63f039a3e963
jsiny <juju.siny@gmail.com>
2019-02-22 11:40:23 +0000

    Add log in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  def hello
-    render html: "Bonjour le monde"
-  end
+  include SessionsHelper
 end
