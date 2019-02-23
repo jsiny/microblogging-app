63e5957fa7826d4ab88af0b1c6ba25ef044db7ff
jsiny <juju.siny@gmail.com>
2019-02-23 12:54:46 +0000

    Add test to prevent log out edge cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert @user.invalid?
   end
+  
+  test "authenticated? should return false for a user with nil remember_digest" do
+    assert_not @user.authenticated?('')
+  end
+  
 end
