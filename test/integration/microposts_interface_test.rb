d241d10cf107dd6f0d9f11740301e9424d600f40
jsiny <juju.siny@gmail.com>
2019-03-01 21:09:31 +0000

    Add micropost sidebar count tests

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -63,4 +63,21 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'a', text: 'delete', count: 0
   end
   
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_url
+    assert_match "#{@user.microposts.count} microposts", response.body
+  end
+  
+  test "micropost sidebar count with 0 post" do
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_url
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "ARCHEEEER!")
+    get root_url
+    assert_match "1 micropost", response.body
+  end  
+  
+  
 end
