2ef528b52c1fa4424abce1a8fab218b0b269a528
jsiny <juju.siny@gmail.com>
2019-03-01 21:30:12 +0000

    Bundle update

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   
   belongs_to :user
   default_scope -> { order(created_at: :desc)}    # Make sure that the newest post comes first
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   
