5e0cfea6d3ed3de1431fc96bd84f0db95af9a672
jsiny <juju.siny@gmail.com>
2019-03-03 07:25:02 +0000

    Add passive relationship (follower) methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   
   has_many :microposts, dependent: :destroy   # If a user is deleted, so are his posts
-  has_many :active_relationships, class_name:  "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
+  has_many :active_relationships,   class_name:   "Relationship",
+                                    foreign_key:  "follower_id",
+                                    dependent:    :destroy
+  has_many :passive_relationships,  class_name:   "Relationship",
+                                    foreign_key:  "followed_id",
+                                    dependent:    :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   
   # Check the adequacy of the email syntax
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
