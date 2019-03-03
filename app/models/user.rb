3851617990a147d42656629d5fc5c01fe6c98ead
jsiny <juju.siny@gmail.com>
2019-03-03 07:19:36 +0000

    Add user test for following

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
-  
+  has_many :following, through: :active_relationships, source: :followed
   
   # Check the adequacy of the email syntax
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -83,6 +83,21 @@ class User < ApplicationRecord
   # Defines a proto-feed
   def feed
     Micropost.where("user_id = ?", id)
+  end
+  
+  # Follows a user
+  def follow(other_user)
+    following << other_user
+  end
+  
+  # Unfollows a user
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+  
+  # Returns true if the current user is following the other user
+  def following?(other_user)
+    following.include?(other_user)
   end
   
     
